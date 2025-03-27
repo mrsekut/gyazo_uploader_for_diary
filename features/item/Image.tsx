a25be72bc81dd8363935742009ba849c0d904f4b
mrsekut <k.cloudspider@gmail.com>
2025-03-27 18:51:05 +0900

    loading

--- features/item/Image.tsx
+++ features/item/Image.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import NextImage from 'next/image';
 import heic2any from 'heic2any';
+import { Loader } from 'lucide-react';
 
 type Props = {
   file: File;
@@ -8,15 +9,19 @@ type Props = {
 
 export const Image = ({ file }: Props) => {
   const [previewUrl, setPreviewUrl] = useState<string | null>(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const processFile = async () => {
       if (isHeic(file)) {
         try {
+          setLoading(true);
           const url = await convertHeicToJpeg(file);
           setPreviewUrl(url);
         } catch (error) {
           console.error('HEIC conversion failed:', error);
+        } finally {
+          setLoading(false);
         }
       } else {
         const url = URL.createObjectURL(file);
@@ -26,6 +31,13 @@ export const Image = ({ file }: Props) => {
 
     processFile();
   }, [file]);
+
+  if (loading)
+    return (
+      <div className="flex items-center justify-center h-full">
+        <Loader className="w-8 h-8 animate-spin" />
+      </div>
+    );
   if (!previewUrl) return null;
 
   return (
